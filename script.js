aa633f6099e50f413f4733a0f3f3d7d9bc1e7477
nicoletur <effigies.noctis@gmail.com>
2021-03-22 09:20:06 -0300

    creacion del terrible

---
+++ script.js
@@ -0,0 +1,18 @@
+let entrada = prompt ("ingresa su nombre");
+let cantidad = prompt ("Cuantas hamburguezas desea")
+let precio = (cantidad * 4500);
+let salida = "hola "+ entrada+ ", llevas " + cantidad + " hamburguezas y su valor es de " + precio;
+alert(salida);
+
+let numero=prompt("ingrese su telefono: ");
+
+document.write( "Estimad@ "+ entrada + ", llevas " + cantidad + " hamburguezas y su valor es de " + precio +  " se le avisara al numero "+ numero + " cuando su pedido este listo" );
+
+console.log( "Estimad@ "+ entrada + ", llevas " + cantidad + " hamburguezas y su valor es de " + precio +  " se le avisara al numero "+ numero + " cuando su pedido este listo" );
+
+
+
+
+
+
+
