4251162cfaaddba69eb8fde02debaf745d5c5ec8
nicoletur <effigies.noctis@gmail.com>
2021-04-20 16:54:15 -0400

    DOM

--- script.js
+++ script.js
@@ -1,43 +1,33 @@
 
-// function datosUsuario (a, b, c) {
-//     this.nombre = a;
-//     this.comuna = b,
-//     this.pago = c;
-//     this.saludar = function(){
-//         console.log("Estimado " + this.nombre + " se ha ingresado su metodo de pago " + this.pago)
-//     }
-// }
- 
-//     let nombre = prompt ("Ingresar Nombre ");
-//     let comuna = prompt (" Ingrese su comuna");
-//     let pago = prompt ("Ingresar tipo de pago en tarjeta o efectivo");
-         
-//     let alumnoLuis = new datosUsuario (nombre, comuna, pago)
-    
-//     console.log(alumnoLuis);
-//     console.log(alumnoLuis.saludar());
-
-
-// const Comuna = [ 2000, 0 , 500, 1000, 1500, 2000]
-// let maipu = Comuna[0] + Comuna[1]; 
-// let cerrillos = Comuna[0] + Comuna[2];
-// let quinta = Comuna[0] + Comuna[3];
-// let prado = Comuna[0] + Comuna[4]
-// let estCentral = Comuna[0] + Comuna[5];
-
-// const excluirCobro = Comuna.slice(1,6)
-
-// for (let i = 0 ; i < Comuna.length ; i ++) {
-//     console.log(Comuna[i]);
-// }
-
-// console.log(Comuna.length);
-// console.log(excluirCobro.length);
+window.onload = function(){
+  var nombreUsuario = prompt("Bienvenido al Terrible, me dices tu nombre?");
+  document.querySelector (".saludoInicial").innerHTML = nombreUsuario;
+}
 
 
 
-const Ingredientes = [ { id: 0 ,  producto: "Hamburguesa", precio: 600},
-                        { id: 1,  producto: "Pan Casero Hamburguesa", precio: 300 },
+var Frases = new Array()
+Frases[0] = "Gracias por visitarnos";
+Frases[1] = "Con que te vamos a deleitar en esta ocasion";
+Frases[2] = "Si es tu primera vez, quedaras con unas ganas Terribles de más";
+Frases[3] = "Bienvenido nuevamente, con que te deleitaremos hoy";
+Frases[4] = "Bienvenido a probar la mejor hamburguesa de la zona Poniente"
+Frases[5] = "Si no sabes que eleguir, prueba con las terribles, te encantaran"
+Frases[6] = "Busca tu hamburgueza, tenemos para todos los gustos"
+
+function aleatorio() {
+  document.querySelector("#resultado").innerHTML = "";//Limpia el mensaje anterior
+  nro = Math.floor(Math.random() * (Frases.length - 0) + 0);//obtienes el valor aleatorio siempre acorde al tamaño de tu array
+  document.querySelector("#resultado").innerHTML = Frases[nro];//imprime resultado
+}
+aleatorio();
+
+
+
+
+
+const Ingredientes = [ { id: 0 ,  producto: "Huevo", precio: 600 },
+                        { id: 1,  producto: "Hamburguesa", precio: 300 },
                         { id: 2,  producto: "Ketchup", precio: 50 },
                         { id: 3,  producto: "Tomate", precio: 50 },
                         { id: 4,  producto: "Mayonesa Casera", precio: 50},
@@ -48,9 +38,21 @@ const Ingredientes = [ { id: 0 ,  producto: "Hamburguesa", precio: 600},
                         { id: 9,  producto: "Lechuga", precio: 20 },
                         { id: 10, producto: "Palta", precio: 200 },
                         { id: 11, producto: "Queso", precio: 100 },
-                        { id: 12, producto: "Pan Completo", precio: 150}]
+                        { id: 12, producto: "Porotos Verdes", precio: 150}]
 
 Ingredientes.push( {id: 13, producto:"Champiñon", precio: 300} , {id:14, producto: "vienesas", precio: 250} )
+
+
+
+for (const ingrediente of Ingredientes) {
+    let contenedor = document.createElement("div");
+    //Definimos el innerHTML del elemento con una plantilla de texto
+    contenedor.innerHTML = `<h4> ${ingrediente.producto}<h4>
+                            <b> $ ${ingrediente.precio}</b>`;
+    document.querySelector ('.ingredientes').appendChild(contenedor);
+}
+
+
 
 const tipoHamburguesa = [ "Hamburguesa Basica", "Hamburguesa Glotona", "Hamburguesa  campestre", "Hamburguesa Royal", "Chacarera", 
                         ]
@@ -62,11 +64,13 @@ const bebidas = bebidasCCU.concat (bebidaCocacola);
 
 console.log(bebidas.join (" - "))
 
+
 console.log(bebidas.indexOf ("Limon Soda"))
 console.log(bebidas.indexOf ("Pepsi"))
 
 console.log(tipoHamburguesa.join(" * "));
 
+//ordenamiento sort
 for (let Ingrediente of Ingredientes) {
     console.log(Ingrediente);
         }
@@ -95,3 +99,9 @@ for (let Ingrediente of Ingredientes) {
               return 0;
           });
           console.log('ordenado por precio:', Ingredientes.sort());
+
+
+
+
+
+  
