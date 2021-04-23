608686bee9d9bd84595721a698af924a4b3a8e2b
nicoletur <effigies.noctis@gmail.com>
2021-04-22 22:30:48 -0400

    event

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
+//captura nombre del usuario y muestra su nombre
 window.onload = function(){
   var nombreUsuario = prompt("Bienvenido al Terrible, me dices tu nombre?");
   document.querySelector (".saludoInicial").innerHTML = nombreUsuario;
 }
 
 
-
+//array con diferentes saludos aleatoreos cuando se carga la pagina
 var Frases = new Array()
 Frases[0] = "Gracias por visitarnos";
 Frases[1] = "Con que te vamos a deleitar en esta ocasion";
@@ -24,7 +25,7 @@ aleatorio();
 
 
 
-
+//array con ingredientes
 
 const Ingredientes = [ { id: 0 ,  producto: "Huevo", precio: 600 },
                         { id: 1,  producto: "Hamburguesa", precio: 300 },
@@ -43,9 +44,9 @@ const Ingredientes = [ { id: 0 ,  producto: "Huevo", precio: 600 },
 Ingredientes.push( {id: 13, producto:"Champiñon", precio: 300} , {id:14, producto: "vienesas", precio: 250} )
 
 
-
+//se cambia la lista por botones que muestran los contenidos
 for (const ingrediente of Ingredientes) {
-    let contenedor = document.createElement("div");
+    let contenedor = document.createElement("button");
     //Definimos el innerHTML del elemento con una plantilla de texto
     contenedor.innerHTML = `<h4> ${ingrediente.producto}<h4>
                             <b> $ ${ingrediente.precio}</b>`;
@@ -53,15 +54,28 @@ for (const ingrediente of Ingredientes) {
 }
 
 
-
+//array con los tipos de hamburguesas
 const tipoHamburguesa = [ "Hamburguesa Basica", "Hamburguesa Glotona", "Hamburguesa  campestre", "Hamburguesa Royal", "Chacarera", 
                         ]
 
+// array con las diferentes bebidas
 const bebidasCCU = [ "Pepsi", "Crush", "Pap", "Bilz" ]
 const bebidaCocacola = [ "Cocacola ", "Fanta", "Sprite"]
 const bebidas = bebidasCCU.concat (bebidaCocacola);
+  
+//se crean botonos para las bebidas
+function agregarBebidas() {
+  for (var i=0; i< bebidas.length; i++){
+    var creaBoton = document.createElement("button");
+    var t = document.createTextNode (bebidas[i]);
+    creaBoton.appendChild(t);
+    document.querySelector(".listaBebidas").appendChild(creaBoton);
+  }
+}
+agregarBebidas();
 
 
+//uso de Join e IndexOf
 console.log(bebidas.join (" - "))
 
 
@@ -83,16 +97,6 @@ for (let Ingrediente of Ingredientes) {
             if (a.producto < b.producto) {
               return -1;
             }
-              return 0;
-          });
-          console.log('ordenado por nombre de producto:', Ingredientes.sort());
-
-
-
-          Ingredientes.sort(function (a, b) {
-            if (a.precio > b.precio) {
-              return 1;
-            }
             if (a.precio < b.precio) {
               return -1;
             }
@@ -104,4 +108,79 @@ for (let Ingrediente of Ingredientes) {
 
 
 
+       // array con formulario que muestra en pantalla datos recopilados despues de presionar el boton
+
+          var nombreCliente, direccionCliente, telefonoCliente, emailCliente; 
+          
+
+          function sub() {
+            var nombre = null;
+            var telefono = null;
+            var correo = null;
+            
+          
+          
+            nombre = document.getElementById("nombre").value;
+            telefono = document.getElementById("telefono").value;
+            correo = document.getElementById("email").value;
+          
+            if ("" != nombre && "" != telefono && "" != correo) {
+              document.getElementById('nom').innerHTML = nombre;
+              document.getElementById('ape').innerHTML = telefono;
+              document.getElementById('mai').innerHTML = correo;
+            } else {
+              alert('Ingrese todos los datos');
+              return false;
+            }
+          }
+          
+          function edad(e) {
+            edadM = Date.parse(Date()) - Date.parse(e.target.value);
+            edads = new Date();
+            edads.setTime(edadM);
+            resultado = edads.getFullYear() - 1970;
+            res = (resultado <= 0) ? 0 : resultado; // Para evitar que sea negativo
+            document.getElementById('eda').innerHTML = res;
+          }
+          
+          
+          // boton que oculta o muestra  la informacion del usuario
+          window.addEventListener('load', init, false);
+          function init() {
+              let div = document.querySelector('#cuerpo');
+              div.style.visibility = 'visible';
+              let boton = document.querySelector('#disparador');
+              boton.addEventListener('click', function (e) {
+                  if(div.style.visibility === 'visible'){
+                      div.style.visibility = 'hidden';
+                  }else{
+                      div.style.visibility = 'visible';
+                  }
+              }, false);
+          }
+
+
   
+
+      
+
+
+// funcion que oculta la imagen y que la muestra si se presiona el boton
+
+          function imagenOculta(id){
+            if (document.getElementById){ 
+            var el = document.getElementById(id); 
+            el.style.display = (el.style.display == 'none') ? 'block' : 'none'; 
+            }
+            }
+            window.onload = function(){
+            imagenOculta('contenido');
+            }
+
+
+  // funcion que valida el enter presionado por el usuario y muestra un alert 
+            function validar(event) {
+              tecla = (document.all) ? event.keyCode : event.which;
+             
+              if (tecla==13) alert ( "ingrediente agregado");
+            }
